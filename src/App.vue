42ee043e5980c6d2e10f6814269c0a04d2f431a3
SanjidHaque <sanjidulhaque@gmail.com>
2020-09-03 22:59:01 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-app id="app">
+        <div class="container-fluid">
+            <Header/>
+            <Todos/>
+        </div>
+    </v-app>
+</template>
+
+<script>
+    import Todos from './components/Todos';
+    import Header from './components/Header';
+
+    export default {
+        name: 'App',
+        components: {
+            Todos,
+            Header
+        }
+    }
+</script>
+
+<style>
+    #app {
+        padding: 0 15px;
+        background-color: azure;
+    }
+</style>
+
